c6a166d6255cd29604b8111136ce290c8931e244
Rutva Rajdev <rutva.rajdev@gmail.com>
2019-07-25 14:49:37 -0400

    Created basketball coach class with setter

---
+++ src/main/java/BasketballCoach.java
@@ -0,0 +1,22 @@
+public class BasketballCoach implements Coach {
+    FortuneService fortuneService;
+
+    BasketballCoach() {
+        System.out.println("Inside Basketball coach default constructor");
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice for 1 hour today";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+
+    public void setFortuneService(FortuneService theFortuneService) {
+        System.out.println("Inside set fortune in basketball coach");
+        this.fortuneService = theFortuneService;
+    }
+}
